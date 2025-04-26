a3d44b4ac8fd506e6da451afc9e2efbeda9178b1
syaf izad aswad <syafizad12@gmail.com>
2025-04-27 01:54:53 +0900

    editeed server.js

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ import { GoogleGenerativeAI } from '@google/generative-ai';
 dotenv.config();
 
 const app = express();
-const PORT = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -35,6 +34,7 @@ app.post('/ask', async (req, res) => {
     }
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 
